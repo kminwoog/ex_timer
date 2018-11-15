c7c9bee97766dd620571daf5c156f5340e82cf29
kminwoog <kminwoog@gmail.com>
2018-11-16 04:57:57 +0900

    * code refactoring
    * change naming `__timers__` to `timers`
    * you can call callback handler when clear(..)

--- lib/ex_timer.ex
+++ lib/ex_timer.ex
@@ -6,7 +6,7 @@ end
 
 defmodule ExTimer do
   @moduledoc """
-  ExTimer module.
+    ExTimer module.
   """
 
   alias ExTimer.Node
@@ -17,30 +17,90 @@ defmodule ExTimer do
   add new timer.
 
   ## Examples
-
-      iex> state = %{ __timers__: [] }
-      iex> state = ExTimer.add(state, {:handler, :name, "uhaha"}, 2000)
-      iex> [node] = state[:__timers__]
-      iex> node.msg == {:handler, :name, "uhaha"}
-      true
-      iex> node.time == 2000
-      true
-
+    iex> state = %{ timers: [] }
+    iex> state = ExTimer.add(state, {:handler, :name, "uhaha"}, 2000)
+    iex> [node] = state.timers
+    iex> node.msg == {:handler, :name, "uhaha"}
+    true
+    iex> node.time == 2000
+    true
   """
   @spec add(state, tuple | atom, integer) :: state
   def add(state, msg, time) when is_tuple(msg) or is_atom(msg) do
-    timers = state[:__timers__]
-    timers = insert(timers, %Node{due: now() + time, time: time, msg: msg})
-    put_in(state[:__timers__], timers)
+    timers =
+      insert(state.timers, %Node{
+        due: now() + time,
+        time: time,
+        msg: msg
+      })
+
+    put_in(state.timers, timers)
   end
 
-  defp insert(nil, timer) do
-    [timer]
+  @doc """
+  delete the previous registerd timer.
+  ## Examples
+    iex> state = %{timers: [%ExTimer.Node{msg: {:handler, :name, "uhaha"}, time: 2000}]}
+    iex> ExTimer.remove(state, {:handler, :name, "uhaha"})
+    %{timers: []}
+  """
+  @spec remove(state, tuple | atom) :: state
+  def remove(state, msg) when is_tuple(msg) or is_atom(msg) do
+    timers = state.timers
+    timers = delete(timers, msg)
+    put_in(state.timers, timers)
   end
 
-  defp insert([], timer) do
-    [timer]
+  @doc """
+  delete all the registerd timers.
+  ## Examples
+    iex> state = %{timers: [%ExTimer.Node{msg: {:handler, :name, "uhaha"}, time: 2000}]}
+    iex> ExTimer.clear(state)
+    %{timers: []}
+  """
+  @spec clear(state, boolean) :: state
+  defmacro clear(state, callback? \\ false) do
+    timer = __ENV__.module
+
+    quote bind_quoted: [state: state, timer: timer, callback?: callback?] do
+      timer.clear_expired(state, __ENV__.module, callback?)
+    end
   end
+
+  def clear_expired(state, caller, callback?) do
+    if callback? do
+      {state, _} =
+        reduce(state.timers, state, 0, fn node, state ->
+          {:noreply, state} = caller.handle_info(node.msg, state)
+          state
+        end)
+
+      put_in(state.timers, [])
+    else
+      put_in(state.timers, [])
+    end
+  end
+
+  defmacro update(state) do
+    timer = __ENV__.module
+
+    quote bind_quoted: [state: state, timer: timer] do
+      timer.update_expired(state, __ENV__.module)
+    end
+  end
+
+  def update_expired(state, caller) do
+    {state, timers} =
+      reduce(state.timers, state, now(), fn node, state ->
+        {:noreply, state} = caller.handle_info(node.msg, state)
+        state
+      end)
+
+    put_in(state.timers, timers)
+  end
+
+  defp insert(nil, timer), do: [timer]
+  defp insert([], timer), do: [timer]
 
   defp insert([h | t] = sorted, timer) do
     if h.time < timer.time do
@@ -50,45 +110,11 @@ defmodule ExTimer do
     end
   end
 
-  @doc false
-  def now() do
+  defp now() do
     :os.system_time(:milli_seconds)
   end
 
-  @doc """
-  delete the previous registerd timer.
-
-  ## Examples
-
-      iex> state = %{__timers__: [%ExTimer.Node{msg: {:handler, :name, "uhaha"}, time: 2000}]}
-      iex> ExTimer.remove(state, {:handler, :name, "uhaha"})
-      %{__timers__: []}
-
-  """
-  @spec remove(state, tuple | atom) :: state
-  def remove(state, msg) when is_tuple(msg) or is_atom(msg) do
-    timers = state[:__timers__]
-    timers = delete(timers, msg)
-    put_in(state[:__timers__], timers)
-  end
-
-  @doc """
-  delete all the registerd timers.
-
-  ## Examples
-
-      iex> state = %{__timers__: [%ExTimer.Node{msg: {:handler, :name, "uhaha"}, time: 2000}]}
-      iex> ExTimer.clear(state)
-      %{__timers__: []}
-
-  """
-  @spec clear(state) :: state
-  def clear(state) do
-    put_in(state[:__timers__], [])
-  end
-
   defp delete(nil, _msg), do: []
-
   defp delete([], _msg), do: []
 
   defp delete([h | t], msg) do
@@ -113,33 +139,14 @@ defmodule ExTimer do
   end
 
   defp equal?(_lhs, _rhs), do: false
+  def reduce(nil, state, _now, _func), do: {state, []}
+  def reduce([], state, _now, _func), do: {state, []}
 
-  # defp equal?(lhs, rhs) when is_tuple(lhs) and is_tuple(rhs) do
-  #   size = tuple_size(lhs)
-  #   size == tuple_size(rhs) and Enum.all?(0..(size - 1), fn i -> elem(lhs, i) == elem(rhs, i) end)
-  # end
-
-  defmacro update(state) do
-    quote bind_quoted: [state: state] do
-      timers = state[:__timers__]
-      ExTimer.expired(state, timers, ExTimer.now(), __ENV__.module)
-    end
-  end
-
-  @doc false
-  def expired(state, nil, _now, _module), do: state
-
-  @doc false
-  def expired(state, [], _now, _module), do: state
-
-  @doc false
-  def expired(state, [h | t], now, module) do
-    if h.due <= now do
-      {:noreply, state} = module.handle_info(h.msg, state)
-      state = put_in(state[:__timers__], t)
-      expired(state, t, now, module)
+  def reduce([h | t] = list, state, now, func) do
+    if now == 0 or h.due <= now do
+      reduce(t, func.(h, state), now, func)
     else
-      state
+      {state, list}
     end
   end
 end
--- test/ex_timer_test.exs
+++ test/ex_timer_test.exs
@@ -4,60 +4,61 @@ defmodule ExTimerTest do
   doctest ExTimer
 
   test "ex_timer" do
-    state = %{calls: 0}
+    # you should be define `timers`(list)
+    state = %{timers: [], calls: 0}
     state = ExTimer.add(state, {:timeout_no_delay, :name, "min"}, 0)
     state = ExTimer.update(state)
     assert state.calls == 1
-    assert length(state.__timers__) == 0
+    assert length(state.timers) == 0
 
     state = ExTimer.add(state, {:timeout_with_delay, :name, "woog"}, 400)
     state = ExTimer.update(state)
     assert state.calls == 1
-    assert length(state.__timers__) != 0
+    assert length(state.timers) != 0
     :timer.sleep(500)
 
     # after sleep for delay
     state = ExTimer.update(state)
     assert state.calls == 2
-    assert length(state.__timers__) == 0
+    assert length(state.timers) == 0
 
     # remove timer with tuple
     state = ExTimer.add(state, {:timer1, :name, "111"}, 400)
-    assert length(state.__timers__) == 1
+    assert length(state.timers) == 1
     state = ExTimer.remove(state, {:timer1, :name})
-    assert length(state.__timers__) == 1
+    assert length(state.timers) == 1
     state = ExTimer.remove(state, {:timer1, "1", "111"})
-    assert length(state.__timers__) == 1
+    assert length(state.timers) == 1
     state = ExTimer.remove(state, {:timer1, :name, "222"})
-    assert length(state.__timers__) == 0
+    assert length(state.timers) == 0
 
     # remove timer with atom
     state = ExTimer.add(state, :timer, 100)
-    assert length(state.__timers__) == 1
+    assert length(state.timers) == 1
     state = ExTimer.remove(state, :no_tuple)
-    assert length(state.__timers__) == 1
+    assert length(state.timers) == 1
     state = ExTimer.remove(state, :timer)
-    assert length(state.__timers__) == 0
+    assert length(state.timers) == 0
 
     # clear timers
     state = ExTimer.add(state, {:timer1, :name, "min1"}, 400)
     state = ExTimer.add(state, {:timer1, :name, "min2"}, 400)
-    assert length(state.__timers__) == 2
+    assert length(state.timers) == 2
     state = ExTimer.clear(state)
-    assert length(state.__timers__) == 0
+    assert length(state.timers) == 0
   end
 
   def handle_info({:timeout_no_delay, arg0, arg1}, state) do
     assert arg0 == :name
     assert arg1 == "min"
-    state = put_in(state[:calls], state.calls + 1)
+    state = put_in(state.calls, state.calls + 1)
     {:noreply, state}
   end
 
   def handle_info({:timeout_with_delay, arg0, arg1}, state) do
     assert arg0 == :name
     assert arg1 == "woog"
-    state = put_in(state[:calls], state.calls + 1)
+    state = put_in(state.calls, state.calls + 1)
     {:noreply, state}
   end
 end
